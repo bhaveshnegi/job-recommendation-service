8b789ca53663116d610e1d6ea4189964c0e821f8
Bhavesh Negi <bhaveshnegi98@gmail.com>
2024-10-06 20:11:43 +0530

    Server created

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const moongoose = require("mongoose");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+moongoose.connect('mongodb+srv://bhaveshnegi:kedarnath@cluster0.flnm3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Job Recommendation Service is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
